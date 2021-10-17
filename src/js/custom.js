74444e6e67862ac53d056a5c6af6b1085b5b5c79
Yuvraj <yuvrajsab@protonmail.com>
2021-10-17 21:09:19 +0530

    change implementation of submenu feature, now using bootstrap

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,2 +1,19 @@
 // Feather icons
-feather.replace({ 'aria-hidden': 'true' });
+feather.replace();
+
+const sidebarSubmenuCollapseElementList = [].slice.call(document.querySelectorAll('#sidebar-menu li.nav-item ul.collapse'));
+sidebarSubmenuCollapseElementList.map(function (collapseElement) {
+	collapseElement.addEventListener('shown.bs.collapse', function () {
+		const collapseLink = this.parentElement.querySelector('a[data-bs-toggle]');
+		const lastItem = collapseLink.firstElementChild.lastElementChild;
+		if (lastItem.tagName === "svg")
+			lastItem.outerHTML = feather.icons['chevron-down'].toSvg({ class: 'm-0' });
+	});
+
+	collapseElement.addEventListener('hidden.bs.collapse', function () {
+		const collapseLink = this.parentElement.querySelector('a[data-bs-toggle]');
+		const lastItem = collapseLink.firstElementChild.lastElementChild;
+		if (lastItem.tagName === "svg")
+			lastItem.outerHTML = feather.icons['chevron-left'].toSvg({ class: 'm-0' });
+	});
+});
