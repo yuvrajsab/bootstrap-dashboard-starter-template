bc5e19b4ead69b7d272ad59487e8e90a1c45b831
Yuvraj <yuvrajsab@protonmail.com>
2021-10-23 22:42:23 +0530

    switch to font-awesome4 from feather-icons

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,19 +1,16 @@
-// Feather icons
-feather.replace();
-
 const sidebarSubmenuCollapseElementList = [].slice.call(document.querySelectorAll('#sidebar-menu li.nav-item ul.collapse'));
 sidebarSubmenuCollapseElementList.map(function (collapseElement) {
 	collapseElement.addEventListener('shown.bs.collapse', function () {
 		const collapseLink = this.parentElement.querySelector('a[data-bs-toggle]');
 		const lastItem = collapseLink.firstElementChild.lastElementChild;
-		if (lastItem.tagName === "svg")
-			lastItem.outerHTML = feather.icons['chevron-down'].toSvg({ class: 'm-0' });
+		if (lastItem.tagName === "I")
+			lastItem.classList.add("fa-rotate-270");
 	});
 
 	collapseElement.addEventListener('hidden.bs.collapse', function () {
 		const collapseLink = this.parentElement.querySelector('a[data-bs-toggle]');
 		const lastItem = collapseLink.firstElementChild.lastElementChild;
-		if (lastItem.tagName === "svg")
-			lastItem.outerHTML = feather.icons['chevron-left'].toSvg({ class: 'm-0' });
+		if (lastItem.tagName === "I")
+			lastItem.classList.remove("fa-rotate-270");
 	});
 });
